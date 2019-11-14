fef4c76d9b7b4dc67e5978179e540a08b8d9bb40
AustinT <austintumlinson@gmail.com>
2019-11-14 10:52:18 -0600

    set state in app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 import List from './List'
 import './App.css';
+import STORE from './STORE';
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
+  state = {
+    store: STORE
   };
 
   render() {
-    const { store } = this.props
     return (
       <main className='App'>
         <header className='App-header'>
           <h1>Trelloyes!</h1>
         </header>
         <div className='App-list'>
-          {store.lists.map(list => (
+          {this.state.store.lists.map(list => (
             <List
               key={list.id}
               header={list.header}
-              cards={list.cardIds.map(id => store.allCards[id])}
+              cards={list.cardIds.map(id => this.state.store.allCards[id])}
             />
           ))}
         </div>
