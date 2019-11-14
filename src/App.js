ffefead4cf62314c9ad190dc772ea79c0c6c80a0
Wendy Bartos <wendyebartos@gmail.com>
2019-11-14 09:46:47 -0800

    adding delete functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import List from './List'
-import './App.css';
-import STORE from './STORE';
+import React, { Component } from "react";
+import List from "./List";
+import "./App.css";
+import STORE from "./STORE";
 
 class App extends Component {
   state = {
     store: STORE
   };
 
+  //this needs to be called when a <card /> delete button clicked
+  handleDeleteClick = id => {
+    console.log("click");
+  };
+
   render() {
     return (
-      <main className='App'>
-        <header className='App-header'>
+      <main className="App">
+        <header className="App-header">
           <h1>Trelloyes!</h1>
         </header>
-        <div className='App-list'>
+        <div className="App-list">
           {this.state.store.lists.map(list => (
             <List
               key={list.id}
               header={list.header}
               cards={list.cardIds.map(id => this.state.store.allCards[id])}
+              handleDelete={this.handleDeleteClick}
             />
           ))}
         </div>
--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,16 @@
+import React from "react";
+import "./Card.css";
 
-import React from 'react';
-import './Card.css';
-
+// access the state here to run the handle click fn
+// create a callback prop in App
 export default function Card(props) {
   return (
-    <div className='Card'>
-      <button
-        type='button'
-      >
+    <div className="Card">
+      <button type="button" onClick={props.handleDelete}>
         delete
       </button>
       <h3>{props.title}</h3>
       <p>{props.content}</p>
     </div>
-  )
+  );
 }
